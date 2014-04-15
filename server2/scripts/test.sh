49a1adb78cee5032185b3b42751e1e589900a82e
okamuuu <okamuuu@gmail.com>
2014-04-15 15:06:06 +0900

    checked enable to post json data to server.

--- server2/scripts/test.sh
+++ server2/scripts/test.sh
@@ -1,7 +1,8 @@
 PORT=8080
-curl -i -X POST -d 'title=test-title&content=test-content' http://localhost:${PORT}/articles
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"title":"test","content":"content"}' http://localhost:8080/articles
 curl -i http://localhost:${PORT}/articles
 curl -i -X PUT -d 'title=testtest&content=hogehogehoge' http://localhost:${PORT}/articles/1
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"title":"test!!","content":"content!!"}' http://localhost:8080/articles
 curl -i http://localhost:${PORT}/articles
 curl -i -X DELETE http://localhost:${PORT}/articles/1
 curl -i http://localhost:${PORT}/articles
