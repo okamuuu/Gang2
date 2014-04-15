b3834fd737b4d96a06cdca518c03a4cbe136e1c8
okamuuu <okamuuu@gmail.com>
2014-04-15 15:01:46 +0900

    I found more simple way to cros.

--- server2/server.js
+++ server2/server.js
@@ -3,17 +3,18 @@ var restify = require('restify');
 var server = restify.createServer(),
     articleSave = require('save')('articlj');
 
-restify.CORS.ALLOW_HEADERS.push('accept');
-restify.CORS.ALLOW_HEADERS.push('sid');
-restify.CORS.ALLOW_HEADERS.push('lang');
-restify.CORS.ALLOW_HEADERS.push('origin');
-restify.CORS.ALLOW_HEADERS.push('withcredentials');
-restify.CORS.ALLOW_HEADERS.push('x-requested-with');
+//restify.CORS.ALLOW_HEADERS.push('accept');
+//restify.CORS.ALLOW_HEADERS.push('sid');
+//restify.CORS.ALLOW_HEADERS.push('lang');
+//restify.CORS.ALLOW_HEADERS.push('origin');
+//restify.CORS.ALLOW_HEADERS.push('withcredentials');
+//restify.CORS.ALLOW_HEADERS.push('x-requested-with');
 
 server
     .use(restify.fullResponse())
     .use(restify.bodyParser())
-    .use(restify.CORS());
+    .use(restify.CORS({'origins': ['http://localhost']}));
+//    .use(restify.CORS());
 
 // search
 server.get('/articles', function(req, res, next) {
